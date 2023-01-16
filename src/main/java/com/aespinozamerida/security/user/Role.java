c7fc19decc46819ff71b17e675a92080f08036df
alvaroEspinozaMerida <aespinozamerida@gmail.com>
2023-01-16 11:05:34 -0800

    initial commit for the Spring 3 tutorial followed

---
+++ src/main/java/com/aespinozamerida/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.aespinozamerida.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/aespinozamerida/security/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.aespinozamerida.security.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
